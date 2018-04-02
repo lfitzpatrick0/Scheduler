671d26f7a93384b2b86be40ba64dbb021b5660ac
Liam <liam@lfitz.net>
2018-04-02 09:15:26 -0700

    Initial commit

---
+++ insert.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+struct listNode {
+	int data;
+	struct listNode *next;
+};
+
+typedef struct listNode ListNode;
+typedef ListNode *ListNodePtr;
+
+void insert( ListNodePtr *sPtr, int data );
+void printList( ListNodePtr currentPtr );
+struct listNode* circular( struct listNode* head );
+
+int main()
+{
+	ListNodePtr startPtr = NULL;
+	insert( &startPtr, 1 );
+	insert( &startPtr, 2 );
+	insert( &startPtr, 4 );
+	insert( &startPtr, 3 );
+	insert( &startPtr, 4 );
+	
+	listNode *scheduler = circular( startPtr );
+	
+	printList( scheduler );
+	return 0;
+}
+
+struct listNode* circular( struct listNode* head )
+{
+	struct listNode* start = head;
+	while(head->next != NULL)
+		head = head->next;
+	head->next = start;
+	return start;
+}
+
+void insert( ListNodePtr *sPtr, int value )
+{
+	ListNodePtr newPtr;
+	ListNodePtr previousPtr;
+	ListNodePtr currentPtr;
+		
+	newPtr = new ListNode;
+	if( newPtr != NULL )
+	{
+		newPtr->data = value;
+		newPtr->next = NULL;
+		
+		previousPtr = NULL;
+		currentPtr = *sPtr;
+		
+		while( currentPtr != NULL && value > currentPtr->data )
+		{
+			previousPtr = currentPtr;
+			currentPtr = currentPtr->next;
+		}
+		if(previousPtr == NULL)
+		{
+			newPtr->next = *sPtr;
+			*sPtr = newPtr;
+		}
+		else
+		{
+			previousPtr->next = newPtr;
+			newPtr->next = currentPtr;
+		}
+	}
+}
+
+void printList( ListNodePtr currentPtr )
+{
+	while( currentPtr != NULL )
+	{
+		cout << currentPtr->data << endl;
+		currentPtr = currentPtr->next;
+	}
+}
