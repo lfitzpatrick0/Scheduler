8a9724f14b03f09cdd57a26506761395990b3e24
Liam Fitzpatrick <liamf9@gmail.com>
2018-04-04 19:23:31 -0700

    process removal is working.

--- insert.cpp
+++ insert.cpp
@@ -1,31 +1,41 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct listNode {
 	int arrival;
 	int burst;
+	string procName;
 	struct listNode *next;
 };
 
 typedef struct listNode ListNode;
 typedef ListNode *ListNodePtr;
 
-void insert( ListNodePtr *sPtr, int data );
+void insert( ListNodePtr *sPtr, int burst, int arrival, string name );
 void printList( ListNodePtr currentPtr );
 struct listNode* circular( struct listNode* head );
+void removeProc(ListNodePtr *sPtr, string procName);
 
 int main()
 {
 	ListNodePtr startPtr = NULL;
-	insert( &startPtr, 1 );
-	insert( &startPtr, 2 );
-	insert( &startPtr, 4 );
-	insert( &startPtr, 3 );
-	insert( &startPtr, 4 );
+	insert( &startPtr, 8, 0, "P1" );
+	insert( &startPtr, 6, 2, "P2" );
+	insert( &startPtr, 1, 2, "P3" );
+	insert( &startPtr, 9, 1, "P4" );
+	insert( &startPtr, 3, 3, "P5" );
+
 	
-	listNode *scheduler = circular( startPtr );
+	// uncomment for circular linked list.
+	//listNode *scheduler = circular( startPtr );
 	
-	printList( scheduler );
+	printList(startPtr);
+	
+	removeProc(&startPtr, "P3");
+	
+	printList(startPtr);
+
 	return 0;
 }
 
@@ -38,7 +48,7 @@ struct listNode* circular( struct listNode* head )
 	return start;
 }
 
-void insert( ListNodePtr *sPtr, int value )
+void insert( ListNodePtr *sPtr, int burst, int arrival, string name )
 {
 	ListNodePtr newPtr;
 	ListNodePtr previousPtr;
@@ -47,13 +57,15 @@ void insert( ListNodePtr *sPtr, int value )
 	newPtr = new ListNode;
 	if( newPtr != NULL )
 	{
-		newPtr->data = value;
+		newPtr->arrival = arrival;
+		newPtr->burst = burst;
+		newPtr->procName = name;
 		newPtr->next = NULL;
 		
 		previousPtr = NULL;
 		currentPtr = *sPtr;
 		
-		while( currentPtr != NULL && value > currentPtr->data )
+		while( currentPtr != NULL && arrival > currentPtr->arrival )
 		{
 			previousPtr = currentPtr;
 			currentPtr = currentPtr->next;
@@ -71,13 +83,41 @@ void insert( ListNodePtr *sPtr, int value )
 	}
 }
 
-//this will loop infinitely
-//add a loop counter
 void printList( ListNodePtr currentPtr )
 {
 	while( currentPtr != NULL )
 	{
-		cout << currentPtr->data << endl;
+		cout << currentPtr->procName << endl;
 		currentPtr = currentPtr->next;
+	}
+}
+
+void removeProc(ListNodePtr *sPtr, string procName)
+{
+	ListNodePtr previousPtr;
+	ListNodePtr currentPtr;
+	ListNodePtr tempPtr;
+
+	if( procName == (*sPtr)->procName )
+	{
+		tempPtr = *sPtr;
+		*sPtr = (*sPtr)->next;
+		free( tempPtr );
+	}
+	else {
+		previousPtr = *sPtr;
+		currentPtr = (*sPtr)->next;
+		while(currentPtr != NULL && currentPtr->procName != procName)
+		{
+			previousPtr = currentPtr;
+			currentPtr = currentPtr->next;
+		}
+		if( currentPtr != NULL )
+		{
+			cout << "removing: " << currentPtr->procName << endl;
+			tempPtr = currentPtr;
+			previousPtr->next = currentPtr->next;
+			free( tempPtr );
+		}
 	}
 }
